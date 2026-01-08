03af8221756b56c8b0c11f9916a77dd73db5aaf3
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2026-01-08 17:33:37 +0700

    implement window - Safari and Resume

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Draggable } from 'gsap/Draggable';
 gsap.registerPlugin(Draggable);
 
 import { Dock, Navbar, Welcome } from '#components';
-import { Terminal } from '#windows';
+import { Resume, Safari, Terminal } from '#windows';
 
 const App = () => {
   return (
@@ -13,6 +13,8 @@ const App = () => {
       <Dock />
 
       <Terminal />
+      <Safari />
+      <Resume />
     </main>
   );
 };
